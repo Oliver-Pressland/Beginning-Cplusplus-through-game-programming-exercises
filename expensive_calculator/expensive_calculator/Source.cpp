a5fd71a3c5f5573d228b67dc5aaa85eb48f45027
OliverPressland <1712908@brunel.ac.uk>
2021-07-01 16:49:14 +0100

    Completed both int and double calculations for "Expensive Calculator", TODO: refactoring

--- expensive_calculator/expensive_calculator/Source.cpp
+++ expensive_calculator/expensive_calculator/Source.cpp
@@ -8,6 +8,8 @@ int main() {
 	std::cout << "Random int 1: " << rint1 << std::endl;
 	std::cout << "Random int 2: " << rint2 << std::endl;
 
+	// Generate random ints for calculator
+
 	std::string add1 = std::to_string(rint1) + " + " + std::to_string(rint2) + " = ";
 	std::string sub1 = std::to_string(rint1) + " - " + std::to_string(rint2) + " = ";
 	std::string mul1 = std::to_string(rint1) + " x " + std::to_string(rint2) + " = ";
@@ -16,8 +18,33 @@ int main() {
 	std::cout << add1 << rint1 + rint2 << std::endl;
 	std::cout << sub1 << rint1 - rint2 << std::endl;
 	std::cout << mul1 << rint1 * rint2 << std::endl;
+
+	// Results as expected
+
 	std::cout << div1 << rint1 / rint2 << std::endl;
-	// Generate random ints for calculator
+
+	// Result unusual as integer division generates integer result (throws decimal away)
+
+	double rdob1 = static_cast <double>(rand() % 10);
+	double rdob2 = static_cast <double>(rand() % 10);
+
+	std::cout << "Random double 1: " << rdob1 << std::endl;
+	std::cout << "Random double 2: " << rdob2 << std::endl;
+
+	// Generate random doubles
+	
+	std::string add2 = std::to_string(rdob1) + " + " + std::to_string(rdob2) + " = ";
+	std::string sub2 = std::to_string(rdob1) + " - " + std::to_string(rdob2) + " = ";
+	std::string mul2 = std::to_string(rdob1) + " x " + std::to_string(rdob2) + " = ";
+	std::string div2 = std::to_string(rdob1) + " / " + std::to_string(rdob2) + " = ";
+
+	std::cout << add2 << rdob1 + rdob2 << std::endl;
+	std::cout << sub2 << rdob1 - rdob2 << std::endl;
+	std::cout << mul2 << rdob1 * rdob2 << std::endl;
+	std::cout << div2 << rdob1 / rdob2 << std::endl;
+
+	// All results as expected
+	// TODO: refactor as methods
 
 	return 0;
 }
