2d4ccf00477645dec66d938de743b05d19447497
OliverPressland <1712908@brunel.ac.uk>
2021-07-01 16:18:38 +0100

    Started the "Expensive Calculator" exercise, added random int generator to 10

---
+++ expensive_calculator/expensive_calculator/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int rint1 = rand() % 10;
+	int rint2 = rand() % 10;
+
+	cout << "Random int 1: " << rint1 << endl;
+	cout << "Random int 2: " << rint2 << endl;
+
+	// Generate random ints for calculator
+
+	return 0;
+}
