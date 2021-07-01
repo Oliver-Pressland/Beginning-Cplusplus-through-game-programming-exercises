4f9fae2a68cfd22ed57314213dc1576e79b7cd99
OliverPressland <1712908@brunel.ac.uk>
2021-07-01 16:35:04 +0100

    Added int calculations for "Expensive Calculator" task

--- expensive_calculator/expensive_calculator/Source.cpp
+++ expensive_calculator/expensive_calculator/Source.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
-using namespace std;
+#include <string>
 
 int main() {
 	int rint1 = rand() % 10;
 	int rint2 = rand() % 10;
 
-	cout << "Random int 1: " << rint1 << endl;
-	cout << "Random int 2: " << rint2 << endl;
+	std::cout << "Random int 1: " << rint1 << std::endl;
+	std::cout << "Random int 2: " << rint2 << std::endl;
 
+	std::string add1 = std::to_string(rint1) + " + " + std::to_string(rint2) + " = ";
+	std::string sub1 = std::to_string(rint1) + " - " + std::to_string(rint2) + " = ";
+	std::string mul1 = std::to_string(rint1) + " x " + std::to_string(rint2) + " = ";
+	std::string div1 = std::to_string(rint1) + " / " + std::to_string(rint2) + " = ";
+
+	std::cout << add1 << rint1 + rint2 << std::endl;
+	std::cout << sub1 << rint1 - rint2 << std::endl;
+	std::cout << mul1 << rint1 * rint2 << std::endl;
+	std::cout << div1 << rint1 / rint2 << std::endl;
 	// Generate random ints for calculator
 
 	return 0;
